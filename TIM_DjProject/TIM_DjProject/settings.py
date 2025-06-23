240058138d4811787c43282dfc7920f12e8c854a
AnesIDAW <anessnaimi29@gmail.com>
2025-06-23 16:02:25 +0200

    Add Email Configuration in settings.py

--- TIM_DjProject/TIM_DjProject/settings.py
+++ TIM_DjProject/TIM_DjProject/settings.py
@@ -243,3 +243,12 @@ CACHES = {
         }
     }
 }
+
+# settings.py
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'naimianes312@gmail.com'
+EMAIL_HOST_PASSWORD = 'nepc syxl fswa wkrg'
+EMAIL_USE_TLS = True
